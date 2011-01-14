8c46d5335999c9bbb047f313ee028beb8d3bea1f
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2011-01-14 13:18:14 +0100

    Also read the cache. That's what it's for.

--- sandbox/cache_benchmark.py
+++ sandbox/cache_benchmark.py
@@ -20,20 +20,29 @@ def main(max_size, new_entries):
     #cache = {}
     # Populate cache until it's full.
     t1 = time.time()
+    L = range(max_size)
+    random.shuffle(L)
     for i in xrange(max_size):
-        rand = random.randint(1, max_size)
+        rand = L[i]
         # The cache checks if entries start with "/".
         cache["/%d" % rand] = rand
     t2 = time.time()
     print_statistics("Filling cache with %d entries" % max_size, t1, t2)
+    # Read the cache.
+    for i in xrange(max_size):
+        data = cache["/%d" % i]
+    t3 = time.time()
+    print_statistics("Reading the cache", t2, t3)
     # Now that the cache is full, try to add 100 more entries,
     # implicitly replacing old entries.
+    L = range(new_entries)
+    random.shuffle(L)
     for i in xrange(new_entries):
-        rand = random.randint(1, new_entries)
+        rand = L[i]
         # Make sure to add entries, not replace them.
         cache["/%d" % (max_size+rand)] = rand
-    t3 = time.time()
-    print_statistics("Replacing %d entries" % new_entries, t2, t3)
+    t4 = time.time()
+    print_statistics("Replacing %d entries" % new_entries, t3, t4)
 
 
 if __name__ == '__main__':
