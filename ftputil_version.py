cb01609ec1f1274eee243d79efcf5341ee2e0178
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2011-03-12 19:17:01 +0100

    Updated version from 2.6b to 2.6.

--- ftputil_version.py
+++ ftputil_version.py
@@ -5,7 +5,7 @@ import sys
 
 
 # ftputil version number; substituted by `make patch`
-__version__ = '2.6b'
+__version__ = '2.6'
 
 _ftputil_version = __version__
 _python_version = sys.version.split()[0]
