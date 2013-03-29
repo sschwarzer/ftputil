9e9a1d956d181d52cdeb9fc419de5fbf3b9433a8
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2013-03-29 22:30:50 +0100

    Commented out debugger invocation.

--- sandbox/test_ticket_71.py
+++ sandbox/test_ticket_71.py
@@ -7,7 +7,7 @@ import ftputil
 ftp_host = ftputil.FTPHost("localhost", "ftptest",
                            "d605581757de5eb56d568a4419f4126e")
 ftp_host._session.set_debuglevel(2)
-import pdb; pdb.set_trace()
+#import pdb; pdb.set_trace()
 ftp_host.listdir("/rootdir2")
 
 print
