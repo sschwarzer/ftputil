6cea4ecf9655a6d0e859ed21ab47e5bbf29cd8a5
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2013-03-31 19:50:51 +0200

    Added module with some helper methods for string encoding/decoding.

---
+++ ftputil/tool.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2013, Stefan Schwarzer
+# See the file LICENSE for licensing terms.
+
+"""
+tool.py - helper code
+"""
+
+from __future__ import unicode_literals
+
+import ftputil.compat as compat
+
+
+__all__ = ["same_string_type_as"]
+
+
+# Encoding to convert a string to the same type as another. This is a
+# "lossless" encoding: Strings can be encoded/decoded back and forth
+# without information loss or causing encoding-related errors. The
+# `ftplib` module under Python 3 also uses the "latin1" encoding.
+ENCODING = "latin1"
+
+
+def same_string_type_as(type_source, content_source):
+    """
+    Return a string of the same type as `type_source` with the content
+    from `content_source`.
+
+    If the `type_source` and `content_source` don't have the same
+    type, use `ENCODING` above to encode or decode, whatever operation
+    is needed.
+    """
+    if (
+      isinstance(type_source, compat.bytes_type) and
+      isinstance(content_source, compat.unicode_type)):
+        return content_source.encode(ENCODING)
+    elif (
+      isinstance(type_source, compat.unicode_type) and
+      isinstance(content_source, compat.bytes_type)):
+        return content_source.decode(ENCODING)
+    else:
+        return content_source
+
+
+def to_bytes_type(string):
+    return same_string_type_as(b"", string)
+
+
+def to_unicode_type(string):
+    return same_string_type_as("", string)
