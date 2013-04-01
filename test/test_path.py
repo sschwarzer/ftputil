9ac8de60c91a6e709fd5f20ac8af1b3b26109501
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2013-04-01 14:44:44 +0200

    Added test whether `abspath` returns the same type its given.

--- test/test_path.py
+++ test/test_path.py
@@ -29,6 +29,14 @@ class SessionWithInaccessibleLoginDirectory(mock_ftplib.MockSession):
 class TestPath(unittest.TestCase):
     """Test operations in `FTPHost.path`."""
 
+    def test_abspath(self):
+        """Test whether the same type as the argument is returned."""
+        host = test_base.ftp_host_factory()
+        self.assertTrue(isinstance(host.path.abspath(b"/"),
+                                   ftputil.compat.bytes_type))
+        self.assertTrue(isinstance(host.path.abspath("/"),
+                                   ftputil.compat.unicode_type))
+
     def test_regular_isdir_isfile_islink(self):
         """Test regular `FTPHost._Path.isdir/isfile/islink`."""
         testdir = '/home/sschwarzer'
