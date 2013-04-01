17195ab6974f1795041aebb3e6c05da1a5ff7cea
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2013-04-01 17:54:30 +0200

    Added function `encode_if_unicode`.
    This function is used for backward compatibility with Python 2.
    In some cases where text is expected and we need a byte string,
    we encode unicode strings but leave byte strings unchanged.

--- test/test_tool.py
+++ test/test_tool.py
@@ -55,5 +55,19 @@ class TestSimpleConversions(unittest.TestCase):
         self.assertTrue(isinstance(result, compat.unicode_type))
 
 
+class TestEncodeIfUnicode(unittest.TestCase):
+
+    def test_do_encode(self):
+        string = "abc"
+        converted_string = ftputil.tool.encode_if_unicode(string, "latin1")
+        self.assertTrue(isinstance(converted_string, compat.bytes_type))
+
+    def test_dont_encode(self):
+        string = b"abc"
+        not_converted_string = ftputil.tool.encode_if_unicode(string, "latin1")
+        self.assertEqual(string, not_converted_string)
+        self.assertTrue(isinstance(not_converted_string, compat.bytes_type))
+
+
 if __name__ == "__main__":
     unittest.main()
