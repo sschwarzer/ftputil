3e377b0ae158f60b895f4dfbee56ad2a4294fd8e
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2013-08-09 22:42:45 +0200

    Updated version in `version.py` to 3.0a.

--- ftputil/version.py
+++ ftputil/version.py
@@ -11,7 +11,7 @@ import sys
 
 
 # ftputil version number; substituted by `make patch`
-__version__ = "2.8"
+__version__ = "3.0a"
 
 _ftputil_version = __version__
 _python_version = sys.version.split()[0]
