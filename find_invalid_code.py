22647c6d8d7a8229a02ca37c6c07855e74687f20
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2013-10-20 21:42:14 +0200

    Use less terse name for unused variable.

--- find_invalid_code.py
+++ find_invalid_code.py
@@ -123,7 +123,7 @@ def main(start_dir):
     """
     # Deliberately shadow global `start_dir`.
     # pylint: disable=redefined-outer-name
-    for dir_path, _, file_names in os.walk(start_dir):
+    for dir_path, _dirnames, file_names in os.walk(start_dir):
         for file_name in file_names:
             abs_name = os.path.abspath(os.path.join(dir_path, file_name))
             if file_name.endswith(".py"):
