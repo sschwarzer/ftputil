3a26e39650d3fb4788ae9efa0cf626096f45b448
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2013-11-03 13:22:15 +0100

    Added `__all__` to `compat`.

--- ftputil/compat.py
+++ ftputil/compat.py
@@ -3,7 +3,7 @@
 # See the file LICENSE for licensing terms.
 
 """
-Functions to make the same code work in both Python 2 and 3.
+Help make the same code work in both Python 2 and 3.
 
 Comments given for the Python 2 versions of the helpers apply to
 the Python 3 helpers as well.
@@ -12,6 +12,10 @@ the Python 3 helpers as well.
 from __future__ import unicode_literals
 
 import sys
+
+
+__all__ = ["int_types", "unicode_type", "bytes_type", "bytes_from_ints",
+           "default_string_type"]
 
 
 python_version = sys.version_info[0]
