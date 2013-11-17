5e6a2f5ce44b11c10746811c6c8738a7215e5961
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2013-11-17 19:25:24 +0100

    Updated version number in several files.

--- ftputil/version.py
+++ ftputil/version.py
@@ -11,7 +11,7 @@ import sys
 
 
 # ftputil version number; substituted by `make patch`
-__version__ = "3.0b"
+__version__ = "3.0"
 
 _ftputil_version = __version__
 _python_version = sys.version.split()[0]
