00f8d89c9e6fd534723e204837591d9a33cab381
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2014-04-21 12:04:51 +0200

    Added unit tests for `session_factory` helper.
    Most tests are done with a mock class, but there's also a test in
    `test_real_ftp.py` that will use `M2Crypto.ftpslib.FTP_TLS` to check
    if the workaround in the `Session` class in
    `ftputil.session.session_factory` works.
    
    Note that since M2Crypto isn't available for Python 3, the real
    FTP test is skipped for Python 3. (This requires the `unittest2`
    module or here the `unittest` in Python 2.7 and 3.3.)

---
+++ test/test_session.py
@@ -0,0 +1,126 @@
+# Copyright (C) 2014, Stefan Schwarzer
+
+"""
+Unit tests for session factory helpers.
+"""
+
+from __future__ import unicode_literals
+
+import unittest
+
+import ftputil.session
+
+
+class MockSession(object):
+    """
+    Mock session base class to determine if all expected calls
+    have happened.
+    """
+
+    def __init__(self):
+        self.calls = []
+    
+    def add_call(self, *args):
+        self.calls.append(args)
+
+    def connect(self, host, port):
+        self.add_call("connect", host, port)
+
+    def auth_tls(self):
+        self.add_call("auth_tls")
+
+    def _fix_socket(self):
+        self.add_call("_fix_socket")
+
+    def set_debuglevel(self, value):
+        self.add_call("set_debuglevel", value)
+
+    def login(self, user, password):
+        self.add_call("login", user, password)
+
+    def set_pasv(self, flag):
+        self.add_call("set_pasv", flag)
+
+    def prot_p(self):
+        self.add_call("prot_p")
+
+
+class TestSessionFactory(unittest.TestCase):
+    """
+    Test if session factories created by
+    `ftputil.session.session_factory` trigger the expected calls.
+    """
+
+    def test_defaults(self):
+        """Test defaults (apart from base class)."""
+        factory = \
+          ftputil.session.session_factory(base_class=MockSession)
+        session = factory("host", "user", "password")
+        self.assertEqual(session.calls, [("connect", "host", 21),
+                                         ("login", "user", "password")])
+
+    def test_different_port(self):
+        """Test setting the command channel port with `port`."""
+        factory = \
+          ftputil.session.session_factory(base_class=MockSession, port=2121)
+        session = factory("host", "user", "password")
+        self.assertEqual(session.calls, [("connect", "host", 2121),
+                                         ("login", "user", "password")])
+
+    def test_use_passive_mode(self):
+        """
+        Test explicitly setting passive/active mode with
+        `use_passive_mode`.
+        """
+        # Passive mode
+        factory = ftputil.session.session_factory(base_class=MockSession,
+                                                  use_passive_mode=True)
+        session = factory("host", "user", "password")
+        self.assertEqual(session.calls, [("connect", "host", 21),
+                                         ("login", "user", "password"),
+                                         ("set_pasv", True)])
+        # Active mode
+        factory = ftputil.session.session_factory(base_class=MockSession,
+                                                  use_passive_mode=False)
+        session = factory("host", "user", "password")
+        self.assertEqual(session.calls, [("connect", "host", 21),
+                                         ("login", "user", "password"),
+                                         ("set_pasv", False)])
+
+    def test_encrypt_data_channel(self):
+        """Test request to call `prot_p` with `encrypt_data_channel`."""
+        factory = ftputil.session.session_factory(base_class=MockSession,
+                                                  encrypt_data_channel=True)
+        session = factory("host", "user", "password")
+        self.assertEqual(session.calls, [("connect", "host", 21),
+                                         ("login", "user", "password"),
+                                         ("prot_p",)])
+
+    def test_debug_level(self):
+        """Test setting the debug level on the session."""
+        factory = ftputil.session.session_factory(base_class=MockSession,
+                                                  debug_level=1)
+        session = factory("host", "user", "password")
+        self.assertEqual(session.calls, [("connect", "host", 21),
+                                         ("set_debuglevel", 1),
+                                         ("login", "user", "password")])
+
+    def test_m2crypto_session(self):
+        """Test call sequence for M2Crypto session."""
+        factory = ftputil.session.session_factory(base_class=MockSession)
+        # Return `True` to fake that this is a session deriving from
+        # `M2Crypto.ftpslib.FTP_TLS`.
+        factory._use_m2crypto_ftpslib = lambda self: True
+        # Override `_fix_socket` here, not in `MockSession`. Since
+        # the created session class _inherits_ from `MockSession`,
+        # it would override the `_fix_socket` there.
+        factory._fix_socket = lambda self: self.add_call("_fix_socket")
+        session = factory("host", "user", "password")
+        self.assertEqual(session.calls, [("connect", "host", 21),
+                                         ("auth_tls",),
+                                         ("_fix_socket",),
+                                         ("login", "user", "password")])
+
+
+if __name__ == "__main__":
+    unittest.main()
