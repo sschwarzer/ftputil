393c0d25b36cc09b9855b632cf783601e99af26f
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2016-07-26 21:12:39 +0200

    Adapt `test_tool.py` for pytest
    ticket: 98

--- test/test_tool.py
+++ test/test_tool.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013, Stefan Schwarzer
+# Copyright (C) 2013-2016, Stefan Schwarzer
 # and ftputil contributors (see `doc/contributors.txt`)
 # See the file LICENSE for licensing terms.
 
@@ -18,42 +18,42 @@ class TestSameStringTypeAs(unittest.TestCase):
 
     def test_to_bytes(self):
         result = ftputil.tool.same_string_type_as(b"abc", "def")
-        self.assertEqual(result, b"def")
-        self.assertTrue(isinstance(result, compat.bytes_type))
+        assert result == b"def"
+        assert isinstance(result, compat.bytes_type)
 
     def test_to_unicode(self):
         result = ftputil.tool.same_string_type_as("abc", b"def")
-        self.assertEqual(result, "def")
-        self.assertTrue(isinstance(result, compat.unicode_type))
+        assert result == "def"
+        assert isinstance(result, compat.unicode_type)
 
     def test_both_bytes_type(self):
         result = ftputil.tool.same_string_type_as(b"abc", b"def")
-        self.assertEqual(result, b"def")
-        self.assertTrue(isinstance(result, compat.bytes_type))
+        assert result == b"def"
+        assert isinstance(result, compat.bytes_type)
 
     def test_both_unicode_type(self):
         result = ftputil.tool.same_string_type_as("abc", "def")
-        self.assertEqual(result, "def")
-        self.assertTrue(isinstance(result, compat.unicode_type))
+        assert result == "def"
+        assert isinstance(result, compat.unicode_type)
 
 
 class TestSimpleConversions(unittest.TestCase):
 
     def test_as_bytes(self):
         result = ftputil.tool.as_bytes(b"abc")
-        self.assertEqual(result, b"abc")
-        self.assertTrue(isinstance(result, compat.bytes_type))
+        assert result == b"abc"
+        assert isinstance(result, compat.bytes_type)
         result = ftputil.tool.as_bytes("abc")
-        self.assertEqual(result, b"abc")
-        self.assertTrue(isinstance(result, compat.bytes_type))
+        assert result == b"abc"
+        assert isinstance(result, compat.bytes_type)
         
     def test_as_unicode(self):
         result = ftputil.tool.as_unicode(b"abc")
-        self.assertEqual(result, "abc")
-        self.assertTrue(isinstance(result, compat.unicode_type))
+        assert result == "abc"
+        assert isinstance(result, compat.unicode_type)
         result = ftputil.tool.as_unicode("abc")
-        self.assertEqual(result, "abc")
-        self.assertTrue(isinstance(result, compat.unicode_type))
+        assert result == "abc"
+        assert isinstance(result, compat.unicode_type)
 
 
 class TestEncodeIfUnicode(unittest.TestCase):
@@ -61,13 +61,13 @@ class TestEncodeIfUnicode(unittest.TestCase):
     def test_do_encode(self):
         string = "abc"
         converted_string = ftputil.tool.encode_if_unicode(string, "latin1")
-        self.assertTrue(isinstance(converted_string, compat.bytes_type))
+        assert isinstance(converted_string, compat.bytes_type)
 
     def test_dont_encode(self):
         string = b"abc"
         not_converted_string = ftputil.tool.encode_if_unicode(string, "latin1")
-        self.assertEqual(string, not_converted_string)
-        self.assertTrue(isinstance(not_converted_string, compat.bytes_type))
+        assert string == not_converted_string
+        assert isinstance(not_converted_string, compat.bytes_type)
 
 
 if __name__ == "__main__":
