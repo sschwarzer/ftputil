28e0c3840506c2a4ecf0494492eda110550195ca
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2018-12-26 20:10:12 +0100

    Remove Python 2-specific code

--- ftputil/__init__.py
+++ ftputil/__init__.py
@@ -44,17 +44,8 @@ Note: ftputil currently is not threadsafe. More specifically, you can
       a single `FTPHost` object in different threads.
 """
 
-import sys
-import warnings
-
-from ftputil.host    import FTPHost
+from ftputil.host import FTPHost
 from ftputil.version import __version__
-
-
-# `sys.version_info.major` isn't available in Python 2.6.
-if sys.version_info[0] == 2:
-    warnings.warn("Python 2 suport will be removed in ftputil 4.0.0",
-                  DeprecationWarning, stacklevel=2)
 
 
 # Apart from `ftputil.error` and `ftputil.stat`, this is the whole
