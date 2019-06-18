6c71c6841a5a47cc90922ffe136e032dac61aa13
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2019-06-18 18:46:53 +0200

    Use `ScriptedSession` in `TestHostContextManager`

--- test/test_with_statement.py
+++ test/test_with_statement.py
@@ -1,14 +1,18 @@
-# Copyright (C) 2008-2018, Stefan Schwarzer <sschwarzer@sschwarzer.net>
+# Copyright (C) 2008-2019, Stefan Schwarzer <sschwarzer@sschwarzer.net>
 # and ftputil contributors (see `doc/contributors.txt`)
 # See the file LICENSE for licensing terms.
 
+import ftplib
 import pytest
 
 import ftputil.error
 
+import test.scripted_session as scripted_session
 from test import test_base
 from test.test_file import InaccessibleDirSession, ReadMockSession
-from test.test_host import FailOnLoginSession
+
+
+Call = scripted_session.Call
 
 
 # Exception raised by client code, i. e. code using ftputil. Used to
@@ -23,13 +27,24 @@ class ClientCodeException(Exception):
 class TestHostContextManager:
 
     def test_normal_operation(self):
-        with test_base.ftp_host_factory() as host:
+        script = [
+          Call(method_name="__init__"),
+          Call(method_name="pwd", result="/"),
+          Call(method_name="close")
+        ]
+        with test_base.ftp_host_factory(scripted_session.factory(script)) as host:
             assert host.closed is False
         assert host.closed is True
 
     def test_ftputil_exception(self):
+        script = [
+          # Since `__init__` raises an exception, `pwd` isn't called. However,
+          # `close` is called via the context manager.
+          Call(method_name="__init__", result=ftplib.error_perm),
+          Call(method_name="close")
+        ]
         with pytest.raises(ftputil.error.FTPOSError):
-            with test_base.ftp_host_factory(FailOnLoginSession) as host:
+            with test_base.ftp_host_factory(scripted_session.factory(script)) as host:
                 pass
         # We arrived here, that's fine. Because the `FTPHost` object
         # wasn't successfully constructed, the assignment to `host`
@@ -37,8 +52,13 @@ class TestHostContextManager:
         assert "host" not in locals()
 
     def test_client_code_exception(self):
+        script = [
+          Call(method_name="__init__"),
+          Call(method_name="pwd", result="/"),
+          Call(method_name="close")
+        ]
         try:
-            with test_base.ftp_host_factory() as host:
+            with test_base.ftp_host_factory(scripted_session.factory(script)) as host:
                 assert host.closed is False
                 raise ClientCodeException()
         except ClientCodeException:
