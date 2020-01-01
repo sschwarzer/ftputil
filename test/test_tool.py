8d09473b2536adcaff973e6c1704f3b0a8a9de4b
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2020-01-01 01:23:36 +0100

    Support `PathLike` objects in `same_string_type_as`
    If a `PathLike` object is passed as `type_source` argument, the result
    from its `__fspath__` method is used to determine the actual
    `type_source`.

--- test/test_tool.py
+++ test/test_tool.py
@@ -2,25 +2,56 @@
 # and ftputil contributors (see `doc/contributors.txt`)
 # See the file LICENSE for licensing terms.
 
+import os
+
 import ftputil.tool
 
 
+class TypeSourcePath(os.PathLike):
+    """
+    Helper class for `TestSameStringTypeAs`
+    """
+
+    def __init__(self, type_source):
+        self._type_source = type_source
+
+    def __fspath__(self):
+        return self._type_source
+
+
 class TestSameStringTypeAs:
+    @staticmethod
+    def _test_string_and_pathlike_object(type_source, content_source, expected_result):
+        """
+        Check if the results from
+        `tool.same_string_type_as(type_source, content_source)` and
+        `tool.same_string_type_as(TypeSourcePath(type_source), content_source)`
+        both are the same as `expected_result`.
+
+        `TypeSourcePath(type_source)` means that the type source
+        string is wrapped in a `PathLike` object whose `__fspath__`
+        method returns `type_source`.
+
+        `type_source` must be a unicode string or byte string.
+        """
+        result = ftputil.tool.same_string_type_as(type_source, content_source)
+        assert result == expected_result
+        #
+        type_source = TypeSourcePath(type_source)
+        result = ftputil.tool.same_string_type_as(type_source, content_source)
+        assert result == expected_result
+
     def test_to_bytes(self):
-        result = ftputil.tool.same_string_type_as(b"abc", "def")
-        assert result == b"def"
+        self._test_string_and_pathlike_object(b"abc", "def", expected_result=b"def")
 
     def test_to_str(self):
-        result = ftputil.tool.same_string_type_as("abc", b"def")
-        assert result == "def"
+        self._test_string_and_pathlike_object("abc", b"def", expected_result="def")
 
     def test_both_bytes_type(self):
-        result = ftputil.tool.same_string_type_as(b"abc", b"def")
-        assert result == b"def"
+        self._test_string_and_pathlike_object(b"abc", b"def", expected_result=b"def")
 
     def test_both_str_type(self):
-        result = ftputil.tool.same_string_type_as("abc", "def")
-        assert result == "def"
+        self._test_string_and_pathlike_object("abc", "def", expected_result="def")
 
 
 class TestSimpleConversions:
